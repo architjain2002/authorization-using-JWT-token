aa24cea48e01bf113f066fd0abdad1eaf90a226d
Archit Jain <arcyjain2002@gmail.com>
2022-06-17 21:47:34 +0530

    control and route changes of users

---
+++ controller/userController.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const mongoose = require("mongoose");
+var User = require("../model/user.js"); //obtaining the user model as User
+
+// for registering the users
+exports.registerUsers = async function (req, res) {};
+
+// for existing users to login
+exports.loginUsers = async function (req, res) {};

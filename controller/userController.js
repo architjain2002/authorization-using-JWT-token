c797dce52c5ef1da2207998641e0abcf593d3845
Archit Jain <arcyjain2002@gmail.com>
2022-06-18 02:32:59 +0530

    jwt configuration for registeration

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 var User = require("../model/user.js"); //obtaining the user model as User
 
 // for registering the users
@@ -10,43 +12,42 @@ exports.registerUsers = async function (req, res) {
 
     // Validate user input
     if (!(email && password && first_name && last_name)) {
-      res.Sendstatus(400).send("All input is required");
-
-      // check if user already exist
-      // Validate if user exist in our database
-      const oldUser = await User.findOne({ email });
-
-      if (oldUser) {
-        return res.Sendstatus(409).send("User Already Exist. Please Login");
-      }
-
-      //Encrypt user password
-      encryptedPassword = await bcrypt.hash(password, 10);
-
-      // Create user in our database
-      const user = await User.create({
-        first_name,
-        last_name,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-      });
-
-      // Create token
-      const token = jwt.sign(
-        { user_id: user._id, email },
-        process.env.TOKEN_KEY,
-        {
-          expiresIn: "2h",
-        }
-      );
-      // save user token
-      user.token = token;
-
-      // return new user
-      res.Sendstatus(201).json(user);
+      res.status(400).send("All input is required");
     }
+    // check if user already exist
+    // Validate if user exist in our database
+    const oldUser = await User.findOne({ email });
+
+    if (oldUser) {
+      return res.status(409).send("User Already Exist. Please Login");
+    }
+
+    //Encrypt user password
+    encryptedPassword = await bcrypt.hash(password, 10);
+
+    // Create user in our database
+    const user = await User.create({
+      first_name,
+      last_name,
+      email: email.toLowerCase(), // sanitize: convert email to lowercase
+      password: encryptedPassword,
+    });
+
+    // Create token
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      "i-am-a-programmer-interested-in-web-developement",
+      {
+        expiresIn: "2h",
+      }
+    );
+    // save user token
+    user.token = token;
+
+    // return new user
+    res.status(201).json(user);
   } catch (err) {
-    res.Sendstatus(400);
+    res.status(400);
     console.log(err);
   }
 };
--- route/userRoute.js
+++ route/userRoute.js
@@ -3,6 +3,6 @@ const router = express.Router();
 
 var userController = require("../controller/userController.js");
 
-router.get("/register", userController.registerUsers);
+router.post("/register", userController.registerUsers);
 
 module.exports = router;

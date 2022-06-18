0984902d0804c3b4537f93c94cb9393cc52ac19b
Archit Jain <arcyjain2002@gmail.com>
2022-06-18 19:52:23 +0530

    login controller

--- controller/userController.js
+++ controller/userController.js
@@ -53,4 +53,38 @@ exports.registerUsers = async function (req, res) {
 };
 
 // for existing users to login
-exports.loginUsers = async function (req, res) {};
+exports.loginUsers = async function (req, res) {
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+};

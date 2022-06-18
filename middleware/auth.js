22ff7029dd82ddc9480801bc16b2874b56590b20
Archit Jain <arcyjain2002@gmail.com>
2022-06-18 22:24:42 +0530

    added the verification of authorize middleware

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jwt");
+
+exports.authorizeToken = function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer Token
+
+  if (token == null) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+    if (err) {
+      res.sendStatus(403);
+    }
+
+    req.authUser = user;
+    next();
+  });
+};

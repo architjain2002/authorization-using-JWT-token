3c4fab91832f54ab2d5ebce589d4da77ee39b04d
Archit Jain <arcyjain2002@gmail.com>
2022-06-18 22:24:53 +0530

    added post route

--- route/userRoute.js
+++ route/userRoute.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 var userController = require("../controller/userController.js");
-
+var userMiddleware = require("../middleware/auth.js");
 // register users
 router.post("/register", userController.registerUsers);
 
 // login users
-router.post("/login", userController.registerUsers);
+router.post("/login", userController.loginUsers);
 
+// to check for the authorization part
+
+router.get("/post", userMiddleware.authorizeToken, userController.userPosts);
 module.exports = router;

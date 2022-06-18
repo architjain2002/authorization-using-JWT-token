195057a54d876a2fc0f17f76a96d9e03214f9d65
Archit Jain <arcyjain2002@gmail.com>
2022-06-18 19:53:40 +0530

    route for login created

--- route/userRoute.js
+++ route/userRoute.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 var userController = require("../controller/userController.js");
 
+// register users
 router.post("/register", userController.registerUsers);
+
+// login users
+router.post("/login", userController.registerUsers);
 
 module.exports = router;
